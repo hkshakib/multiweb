56c6bd402294583e15dffba13f34a2ee27713122
hkshakib <humayunkibriashakib@gmail.com>
2023-02-15 02:00:32 +0600

    feat: simple smtp send emails view added: feat: send_mail function

--- Sending_email/views.py
+++ Sending_email/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse  # new
+from django.http import HttpResponse
+from django.core.mail import send_mail, mail_admins, BadHeaderError
 
 
 # Create your views here.
 
-def send_mail(request):
-    return HttpResponse("HEllo")
+def send_email(request):
+    try:
+        send_mail('Subject', 'massa ge', 'hkshakib@gmail.com', ['shakib@gmail.com'])
+    except BadHeaderError:
+        pass
+    return HttpResponse("Hello")
