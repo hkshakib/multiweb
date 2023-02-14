9826582c4605b8e0521a4267149150124b97df2f
hkshakib <humayunkibriashakib@gmail.com>
2023-02-15 01:46:41 +0600

    feat: Added SMTP4DEV tools and Configurations of it, Added EmailBackends Configurations on Settings.py

--- multiweb/settings.py
+++ multiweb/settings.py
@@ -123,3 +123,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 2525
+DEFAULT_FROM_EMAIL = 'hkshakib@gmail.com'
